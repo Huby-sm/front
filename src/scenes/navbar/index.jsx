7236af37ff37546ded555b5bdde28f5f5962cbac
Valentin <valentinarpin11@live.fr>
2023-01-31 17:37:14 +0100

    feat(12): create interface navbar

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -22,12 +22,42 @@ const Navbar = () => {
     const theme = useTheme();
     const neutralLight = theme.palette.neutral.light;
     const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLight = theme.palette.primary.light;
+    const alt = theme.palette.background.alt;
+
+    const fullName = `${user.firstName} ${user.lastName}`;
+
 
     return(
-        <div>
-            Navbar
-        </div>
-    )
-}
+        <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+            <FlexBetween gap="1.75rem">
+                <Typography
+                fontWeight="bold"
+                fontSize="clamp(1rem, 2rem, 2.25rem)"
+                color="primary"
+                onClick={() => navigate("/home")}
+                sx={{
+                    "&:hover": {
+                        color: primaryLight,
+                        cursor: "pointer",
+                    },
+                }}
+                >
+                    Huby ✈️
+                </Typography>
+                {isNonMobileScreens && (
+                    <FlexBetween backgroundColor={neutralLight} borderRadius="9px" gap="3rem"
+                                 padding="0.1rem 1.5rem">
+                    <InputBase placeholder="Search..."/>
+                        <IconButton>
+                            <Search/>
+                        </IconButton>
+                    </FlexBetween>
+                )}
+            </FlexBetween>
+        </FlexBetween>
+    );
+};
 
 export default Navbar
